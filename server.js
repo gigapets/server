cf966c0674c834a3cda9cbd4ac33a0e98d4a0a55
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-19 08:37:26 -0700

    added a login endpoint and an authentication function that wil only let a logged in user view infromation on the get request

--- server.js
+++ server.js
@@ -62,6 +62,40 @@ server.get('/gigapets', async(req,res)=>{
     
   });
 
+  function generateToken(user) {
+    const payload = {
+      subject: user.id, 
+      username: user.username
+      
+    };
+    const options = {
+      expiresIn: '1d',
+    };
+    return jwt.sign(payload, secret, options);
+  }
+
+  server.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user); 
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token,
+          });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+
   
       //to be used when completed with new endpoints
   function authentication(req, res, next) {
