61a77cfc4c7f5e7c8886f727ae880300a624bf3a
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-19 09:33:51 -0700

    updated new table with seeds

--- seeds/001-gigapets-main.js
+++ seeds/001-gigapets-main.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('gigapets-main').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('gigapets-main').insert([
+        {id: 1, 'gigapet-name':'sam',breakfast:'omelette','breakfast-score':8,lunch:'burger','lunch-score':3,dinner:'steak','dinner-score':'5'},
+        {id: 2, 'gigapet-name':'sam',breakfast:'waffles','breakfast-score':3,lunch:'N/A','lunch-score':0,dinner:'ice cream','dinner-score':'1'},
+        {id: 3, 'gigapet-name':'sam',breakfast:'omelette','breakfast-score':8,lunch:'burger','lunch-score':3,dinner:'shrimp','dinner-score':'7'}
       ]);
     });
 };
