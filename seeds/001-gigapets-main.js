60460f965c6db9be4d5edc90c54ebb672bb14b85
Estevan <estevan.rodriguez10@yahoo.com>
2019-03-19 12:54:17 -0700

    removed two seeds to make it easier for front end

--- seeds/001-gigapets-main.js
+++ seeds/001-gigapets-main.js
@@ -5,9 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('gigapets-main').insert([
-        {id: 1, 'gigapet-name':'sam',breakfast:'omelette','breakfast-score':8,lunch:'burger','lunch-score':3,dinner:'steak','dinner-score':'5'},
-        {id: 2, 'gigapet-name':'sam',breakfast:'waffles','breakfast-score':3,lunch:'N/A','lunch-score':0,dinner:'ice cream','dinner-score':'1'},
-        {id: 3, 'gigapet-name':'sam',breakfast:'omelette','breakfast-score':8,lunch:'burger','lunch-score':3,dinner:'shrimp','dinner-score':'7'}
+        {id: 1, 'gigapet-name':'sam',breakfast:'omelette','breakfast-score':8,lunch:'burger','lunch-score':3,dinner:'steak','dinner-score':'5'}
       ]);
     });
 };
